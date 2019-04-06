acb96dd24a847838fbe70a02ca6969687fbe2111
Tal Diamant <Taldiamant89@gmail.com>
2019-04-06 03:35:24 +0300

    Updates

---
+++ precache-manifest.40861b6087277628b08ab443c584f409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a77d889f00fd3e6cbc66",
+    "url": "/search4movie/public/index.html/static/js/runtime~main.a77d889f.js"
+  },
+  {
+    "revision": "d76921d38088064752c7",
+    "url": "/search4movie/public/index.html/static/js/main.d76921d3.chunk.js"
+  },
+  {
+    "revision": "d33e3c4122513915e5c6",
+    "url": "/search4movie/public/index.html/static/js/1.d33e3c41.chunk.js"
+  },
+  {
+    "revision": "d76921d38088064752c7",
+    "url": "/search4movie/public/index.html/static/css/main.9b9f4b26.chunk.css"
+  },
+  {
+    "revision": "d33e3c4122513915e5c6",
+    "url": "/search4movie/public/index.html/static/css/1.3a6fea0c.chunk.css"
+  },
+  {
+    "revision": "017b69f516224b2420203c96b25ca2ca",
+    "url": "/search4movie/public/index.html/index.html"
+  }
+];
